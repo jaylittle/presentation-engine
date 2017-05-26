6c390d72de3819a1dd12edaad02fc209ec419c03
Jay Little <jaylittle@jaylittle.com>
2017-05-26 19:50:57 -0400

    Integrated XUnit. Added first crappy Unit Test. Tweaked vscode task config.

---
+++ PEngine.Core.Tests/PostServiceTests.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using PEngine.Core.Data.Interfaces;
+using PEngine.Core.Logic;
+using PEngine.Core.Shared.Models;
+using Xunit;
+using Moq;
+
+namespace pengine.tests
+{
+  public class PostServiceTests
+  {
+    //This test is just a sample - its terribad
+    [Fact]
+    public void PostService_NameValidation()
+    {
+      var postData = new PostModel()
+      {
+        Data = "Blah Blah Blah"
+      };
+
+      var mockedPostDal = new Mock<IPostDal>();
+
+      mockedPostDal.Setup(pd => pd.InsertPost(postData));
+      mockedPostDal.Setup(pd => pd.UpdatePost(postData));
+      var postService = new PostService(mockedPostDal.Object);
+      
+      //Verify that record with no name/title is rejected
+      var errors = new List<string>();
+      Assert.True(
+        !postService.UpsertPost(postData, ref errors) &&
+        errors.Any(e => string.Equals("Post Title is a required field", e, StringComparison.OrdinalIgnoreCase))
+      );
+
+      //Verify that record with name/title is accepted
+      errors.Clear();
+      postData.Name = "Crap Title";
+      Assert.True(postService.UpsertPost(postData, ref errors));
+    }
+  }
+}
