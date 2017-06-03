18f3a42f9f3f804b6892cca451b3bb7003c8b768
Jay Little <jaylittle@jaylittle.com>
2017-06-03 16:30:28 -0400

    Tweaked MD5 Hash detection logic just a bit more

--- PEngine.Core.Web/Controllers/HashController.cs
+++ PEngine.Core.Web/Controllers/HashController.cs
@@ -11,7 +11,7 @@ namespace PEngine.Core.Web.Controllers
   [Route("[controller]")]
   public class HashController : Controller
   {
-    private Regex _md5HashRegex = new Regex(@"(?:[0-9]|[A-F]|[a-f]){16}");
+    private Regex _md5HashRegex = new Regex(@"(?:[0-9]|[A-F]){32}");
     public HashController()
     {
     }
@@ -29,7 +29,7 @@ namespace PEngine.Core.Web.Controllers
           case "map":
             break;
           default:
-            originalPath = String.Join(".", elements.Where((e) => !_md5HashRegex.IsMatch(e)));
+            originalPath = String.Join(".", elements.Where((e) => _md5HashRegex.Matches(e).Count != 1));
             break;
         }
         string originalFullPath = System.IO.Path.Combine(Startup.ContentRootPath, "wwwroot", originalPath);
