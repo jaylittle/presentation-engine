f7eb642d5fae663654539eddacc88a94090918c3
Jay Little <jaylittle@jaylittle.com>
2017-06-03 19:29:07 -0400

    Modified hash cache URL generation logic to use built-in URL construction methods

--- PEngine.Core.Web/Controllers/HashController.cs
+++ PEngine.Core.Web/Controllers/HashController.cs
@@ -5,10 +5,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.StaticFiles;
 using PEngine.Core.Shared;
 using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.WebUtilities;
 
 namespace PEngine.Core.Web.Controllers
 {
-  [Route("[controller]")]
+  [Route("hash")]
   public class HashController : Controller
   {
     private Regex _md5HashRegex = new Regex(@"(?:[0-9]|[A-F]){32}");
@@ -19,6 +20,7 @@ namespace PEngine.Core.Web.Controllers
     [HttpGet("{hash}/{*filePath}")]
     public IActionResult GetHashedFileName(string hash, string filePath)
     {
+      filePath = System.Net.WebUtility.UrlDecode(filePath);
       if (_md5HashRegex.Matches(hash).Count == 1 && !string.IsNullOrWhiteSpace(filePath))
       {
         string fullPath = System.IO.Path.Combine(Startup.ContentRootPath, "wwwroot", filePath);
--- PEngine.Core.Web/HtmlHelpers.cs
+++ PEngine.Core.Web/HtmlHelpers.cs
@@ -6,16 +6,22 @@ using System.Collections.Concurrent;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
 using PEngine.Core.Shared;
 
 namespace PEngine.Core.Web
 {
   public static class HtmlHelpers
   {
-    public static HtmlString ContentHashFile(this IHtmlHelper HtmlHelper, string webPath)
+    public static HtmlString ContentHashFile(this IHtmlHelper htmlHelper, string webPath)
     {
+      var urlHelper = new UrlHelper(htmlHelper.ViewContext);
       var cacheEntry = ContentHash.GetContentHashEntryForFile(Startup.ContentRootPath, "wwwroot", webPath);
-      return new HtmlString($"hash/{cacheEntry.Hash}/{cacheEntry.WebPath}");
+      var hashUrl = System.Net.WebUtility.UrlDecode(urlHelper.Action("GetHashedFileName", "hash", new {
+        hash = cacheEntry.Hash,
+        filePath = cacheEntry.WebPath
+      }));
+      return new HtmlString(hashUrl);
     }
   }
 }
