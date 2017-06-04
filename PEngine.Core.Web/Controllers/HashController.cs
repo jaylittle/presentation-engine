cc54dc6f3d6f0bb5ad4d13cf57f461186ecaf67b
Jay Little <jaylittle@jaylittle.com>
2017-06-04 08:55:06 -0400

    Resolved security bug in hash controller that allowed users to requested absolute URLs by abusing %2f forward slash URL encoding

--- PEngine.Core.Web/Controllers/HashController.cs
+++ PEngine.Core.Web/Controllers/HashController.cs
@@ -13,6 +13,8 @@ namespace PEngine.Core.Web.Controllers
   public class HashController : Controller
   {
     private Regex _md5HashRegex = new Regex(@"(?:[0-9]|[A-F]){32}");
+    private Regex _filePathRegex = new Regex(@"^[\w\d]");
+    
     public HashController()
     {
     }
@@ -20,8 +22,10 @@ namespace PEngine.Core.Web.Controllers
     [HttpGet("{hash}/{*filePath}")]
     public IActionResult GetHashedFileName(string hash, string filePath)
     {
+      Console.WriteLine($"Hash Request Received for: {filePath}");
       filePath = System.Net.WebUtility.UrlDecode(filePath);
-      if (_md5HashRegex.Matches(hash).Count == 1 && !string.IsNullOrWhiteSpace(filePath) && !filePath.Contains(".."))
+      if (_md5HashRegex.Matches(hash).Count == 1 && !string.IsNullOrWhiteSpace(filePath) 
+        && !filePath.Contains("..") && _filePathRegex.IsMatch(filePath))
       {
         var hashEntry = ContentHash.GetContentHashEntryForFile(Startup.ContentRootPath, "wwwroot", filePath, true);
         if (hashEntry != null)
