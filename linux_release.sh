88c01495d5139d329ead961987f26206d0ec5eb0
Jay Little <jaylittle@jaylittle.com>
2018-09-18 08:01:34 -0400

    Added Release build script

---
+++ linux_release.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then
+    echo "Must provide version number as an argument"
+    exit 1
+fi
+
+RELEASE=pengine-v$1
+RELEASE_DIR=./$RELEASE
+RELEASE_ARCHIVE=$RELEASE.tgz
+
+if [ -d $RELEASE_DIR ]
+then
+  rm -rf $RELEASE_DIR
+fi
+
+if [ -f $RELEASE_ARCHIVE ]
+then
+  rm $RELEASE_ARCHIVE
+fi
+
+./linux-build.sh
+
+mkdir $RELEASE_DIR
+tar -C $RELEASE_DIR -xf ./pengine_current.tgz
+tar -czvf $RELEASE_ARCHIVE $RELEASE_DIR
