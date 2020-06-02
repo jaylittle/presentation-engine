5ec798f8b5987f996b17e957ab616ed9ec1f1d1a
Jay Little <jaylittle@jaylittle.com>
2020-06-02 12:21:10 -0400

    Completed functional port of article editor to react

--- PEngine.Core.Web/scripts/pengine.core.web.article.editor.react.js
+++ PEngine.Core.Web/scripts/pengine.core.web.article.editor.react.js
@@ -12,7 +12,7 @@ class PEngineArticleEditor extends React.Component {
         name: 'New Article',
         description: '',
         category: '',
-        contentUrl: '',
+        contentURL: '',
         defaultSection: '',
         visibleFlag: false,
         noIndexFlag: true,
@@ -85,7 +85,7 @@ class PEngineArticleEditor extends React.Component {
         name: 'New Article',
         description: '',
         category: '',
-        contentUrl: '',
+        contentURL: '',
         defaultSection: '',
         visibleFlag: false,
         noIndexFlag: true,
@@ -143,6 +143,17 @@ class PEngineArticleEditor extends React.Component {
     }
     this.setState(prevState => {
       prevState.article[fieldName] = fieldValue;
+      return prevState;
+    });
+  }
+
+  updateArticleSectionField = (e, key, fieldName) => {
+    let fieldValue = e.target.value;
+    if (e.target.type === 'checkbox' || e.target.type === 'radio') {
+      fieldValue = e.target.checked;
+    }
+    this.setState(prevState => {
+      prevState.article.sections[key][fieldName] = fieldValue;
       return prevState;
     });
   }
@@ -221,6 +232,68 @@ class PEngineArticleEditor extends React.Component {
     document.body.style.overflow = 'initial';
   }
 
+  addSection = (e) => {
+    this.setState((prevState) => ({
+      article: {
+        ...prevState.article,
+        sections: [
+          ...prevState.article.sections,
+          {
+            name: 'New Section',
+            data: '',
+            sortOrder: 0,
+            uniqueName: null,
+            createdUTC: null,
+            modifiedUTC: null,
+          }
+        ]
+      }
+    }));
+  }
+
+  deleteSectionConfirm = (e, sectionIndex) => {
+    let confirmMessage = `Are you sure you want to delete this section?`;
+    if (this.state.article.sections[sectionIndex] && this.state.article.sections[sectionIndex].name) {
+      confirmMessage = `Are you sure you want to delete this section entitled "${this.state.article.sections[sectionIndex].name}"?`;
+    }
+    if (confirm(confirmMessage)) {
+      return this.deleteSection(e, sectionIndex);
+    }
+  }
+
+  deleteSection = (e, sectionIndex) => {
+    this.setState(prevState => {
+      if (sectionIndex >= 0) {
+        prevState.article.sections.splice(sectionIndex, 1);
+      }
+      return prevState;
+    });
+  }
+
+  moveSection = (e, sectionIndex, offset) => {
+    this.setState(prevState => {
+      if (sectionIndex >= 0) {
+        let sectionObject = prevState.article.sections[sectionIndex];
+        let newSectionIndex = sectionIndex + offset;
+        prevState.article.sections.splice(sectionIndex, 1);
+        prevState.article.sections.splice(newSectionIndex, 0, sectionObject);
+        for (let currentSectionIndex in prevState.article.sections) {
+          prevState.article.sections[currentSectionIndex].sortOrder = currentSectionIndex;
+        }
+      }
+      return prevState;
+    });
+  }
+
+  toggleSection = (e, sectionIndex) => {
+    this.setState(prevState => {
+      if (sectionIndex >= 0) {
+        prevState.article.sections[sectionIndex].expanded = !prevState.article.sections[sectionIndex].expanded;
+      }
+      return prevState;
+    });
+  }
+
   render = () => {
     return (
       <div id="pengine-article-editor-content">
@@ -252,7 +325,111 @@ class PEngineArticleEditor extends React.Component {
               }
               <div>
                 <div className="form-container">
+                  <div className="edit-row">
+                    <div className="edit-label">Title:</div>
+                    <div className="edit-field">
+                      <input type="text" className="edit-control-large" value={this.state.article.name} onChange={(e) => this.updateArticleField(e, 'name')} />
+                    </div>
+                  </div>
+                  <div className="edit-row">
+                    <div className="edit-label">Description:</div>
+                    <div className="edit-field">
+                      <textarea className="edit-control" rows="3" value={this.state.article.description} onChange={(e) => this.updateArticleField(e, 'description')}></textarea>
+                    </div>
+                  </div>
+                  <div className="edit-row">
+                    <div className="edit-label">Content URL:</div>
+                    <div className="edit-field">
+                      <input type="text" className="edit-control-large" value={this.state.article.contentURL} onChange={(e) => this.updateArticleField(e, 'contentURL')} />
+                    </div>
+                  </div>
+                  <div className="edit-row">
+                    <div className="edit-label">Category:</div>
+                    <div className="edit-field">
+                      <input type="text" className="edit-control-large" value={this.state.article.category} onChange={(e) => this.updateArticleField(e, 'category')} />
+                    </div>
+                  </div>
+                  <div className="edit-row">
+                    <div className="edit-label">Default Section:</div>
+                    <div className="edit-field">
+                      <select className="edit-control-large" value={this.state.article.defaultSection} onChange={(e) => this.updateArticleField(e, 'defaultSection')}>
+                        <option value="">[Not Specified]</option>
+                        {
+                          this.state.article.sections.map((section, key) => 
+                            <option key={key} value={section.name}>{ section.name }</option>
+                          )
+                        }
+                      </select>
+                    </div>
+                  </div>
+                  <div className="edit-row">
+                    <div className="edit-label">Visible:</div>
+                    <input type="checkbox" checked={this.state.article.visibleFlag} onChange={(e) => this.updateArticleField(e, 'visibleFlag')} />
+                  </div>
+                  <div className="edit-row">
+                    <div className="edit-label">Do Not Index:</div>
+                    <input type="checkbox" checked={this.state.article.noIndexFlag} onChange={(e) => this.updateArticleField(e, 'noIndexFlag')} />
+                  </div>
+                  <div className="edit-row">
+                    <div className="edit-label">Hide Buttons:</div>
+                    <input type="checkbox" checked={this.state.article.hideButtonsFlag} onChange={(e) => this.updateArticleField(e, 'hideButtonsFlag')} />
+                  </div>
+                  <div className="edit-row">
+                    <div className="edit-label">Hide Dropdown:</div>
+                    <input type="checkbox" checked={this.state.article.hideDropDownFlag} onChange={(e) => this.updateArticleField(e, 'hideDropDownFlag')} />
+                  </div>
+                  <div className="edit-row">
+                    <div className="edit-label">Article Functions:</div>
+                    <div className="edit-field">
+                      <button type="button" id="section_edit_button_add" onClick={(e) => this.addSection(e)}>Add Section</button>
+                    </div>
+                  </div>
                 </div>
+                {
+                  this.state.article.sections.map((section, key) => 
+                    <div className="form-container border-top" key={key}>
+                      <div className="edit-row">
+                        <div className="edit-label">Section Name:</div>
+                        <div className="edit-field">
+                          <input type="text" className="edit-control-large" value={section.name} onChange={(e) => this.updateArticleSectionField(e, key, 'name')}  />
+                        </div>
+                      </div>
+                      <div className="edit-row">
+                        <div className="edit-label">Section Functions:</div>
+                        <div className="edit-field">
+                          {
+                            key !== 0 ?
+                            <button type="button" onClick={(e) => this.moveSection(e, key, -1)}>Move Up</button>
+                            : null
+                          }
+                          {
+                            key !== (this.state.article.sections.length - 1) ?
+                            <button type="button" onClick={(e) => this.moveSection(e, key, 1)}>Move Down</button>
+                            : null
+                          }
+                          {
+                            section.expanded ?
+                            <button type="button" onClick={(e) => this.toggleSection(e, key)}>Hide</button>
+                            :
+                            <button type="button" onClick={(e) => this.toggleSection(e, key)}>Show</button>
+                          }
+                          <button type="button" id="section_edit_button_delete" onClick={(e) => this.deleteSectionConfirm(e, key)}>Delete</button>
+                        </div>
+                      </div>
+                      {
+                        section.expanded ?
+                        <div className="edit-row">
+                          <div className="edit-label">Content:</div>
+                          <div className="edit-field">
+                            <textarea rows="20" className="edit-control" value={section.data} onChange={(e) => this.updateArticleSectionField(e, key, 'data')}></textarea>
+                          </div>
+                        </div>
+                        : null
+                      }
+                    </div>
+                  )
+                }
+                
               </div>
               <div className="panel">
                 <div className="panel-right">
