8339871cb564b05d5181e518eabb53df728d7e44
Jay Little <jaylittle@jaylittle.com>
2023-12-28 10:58:55 -0500

    Removed left over line of pre-lockdown mode filtering code from post service

--- PEngine.Core.Logic/PostService.cs
+++ PEngine.Core.Logic/PostService.cs
@@ -30,8 +30,7 @@ namespace PEngine.Core.Logic
           isAdmin ||
           (!isLockedDown && (p.VisibleFlag && (isView || !p.NoIndexFlag))) ||
           (isLockedDown && (p.LockDownVisibleFlag && (isView || !p.NoIndexFlag)))
-        )
-        .Where(p => isAdmin || (p.VisibleFlag && (isView || !p.NoIndexFlag)));
+        );
     }
 
     public async Task<(IEnumerable<PostModel> exact, IEnumerable<PostModel> fuzzy)> SearchPosts(string searchQuery, string[] searchTerms, bool isAdmin, bool isLockedDown)
